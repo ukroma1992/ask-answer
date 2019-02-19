2c4f0ccab61ac0516c6de1d5e38a188c74d5b082
ukroma1992 <ukroma1992@gmail.com>
2019-02-19 19:54:07 +0200

    Add color picker for user background

---
+++ db/migrate/20190219170418_add_background_color_to_users.rb
@@ -0,0 +1,5 @@
+class AddBackgroundColorToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :background_color, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_142248) do
+ActiveRecord::Schema.define(version: 2019_02_19_170418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_142248) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "background_color"
   end
 
   add_foreign_key "questions", "users"
